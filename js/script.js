077e828293fefab264940dcf0be492186aaea535
Dinh <seredinh@34363bce4f2a.ant.amazon.com>
2018-11-02 12:37:04 -0700

    second-exercise

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ var myName = "Serena";
 var myAge = 25;
 var message = myName + ' is' + myAge;
 alert (message);
+
+//Exercise 2
+//write a function 
+function sayHello () {
+	var response = prompt ("What is your name?");
+	alert ("Hello" + response + "!");
+}
+//to activate the function, have to call a function 
+sayHello ();
