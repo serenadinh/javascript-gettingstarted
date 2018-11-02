916846efbdd39b6217ae1b40a8f9700412f8cb4c
Dinh <seredinh@34363bce4f2a.ant.amazon.com>
2018-11-02 12:41:15 -0700

    lesson2-exercise2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-// Exercise 1
+// Lesson 1
 var myName = "Serena";
 var myAge = 25;
 var message = myName + ' is' + myAge;
 alert (message);
 
-//Exercise 2
-//write a function 
+//Lesson 2
+// Exercise 1: Write a function 
 function sayHello () {
 	var response = prompt ("What is your name?");
 	alert ("Hello" + response + "!");
 }
 //to activate the function, have to call a function 
 sayHello ();
+
+//Exercise 2: 
+function sayGreeting (greeting) {
+	var response = prompt("What is your name?");
+	alert (greeting + "," + response + "!");
+}
+sayGreeting("Good morning"); // passing information into a function is also known as passing an argument to a function 
